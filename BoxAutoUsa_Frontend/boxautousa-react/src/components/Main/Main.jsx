9aa945d3ffad3c822ab540f89a906f071dd08c9c
Vladyslav <5vladik068@gmail.com>
2023-07-15 12:10:45 +0300

    Main Content

--- BoxAutoUsa_Frontend/boxautousa-react/src/components/Main/Main.jsx
+++ BoxAutoUsa_Frontend/boxautousa-react/src/components/Main/Main.jsx
@@ -1,9 +1,11 @@
 import MainSearch from "./MainSearch/MainSearch"
+import MainContent from "./MainContent/MainContent"
 
 export default function Main() {
     return(
         <main>
         <MainSearch />
+        <MainContent />
         </main>
     )
 }
---
+++ BoxAutoUsa_Frontend/boxautousa-react/src/components/Main/MainContent/MainContent.jsx
@@ -0,0 +1,96 @@
+export default function MainContent() {
+    return(
+        <section
+                id="main-content"
+                class="layout-section"
+                aria-labelledby="heading-portfolio"
+            >
+                <div class="container">
+                    <div class="main-content">
+                        <img
+                            src="src/assets/car1.jpeg"
+                            width="300"
+                            height="300"
+                            alt="Car 1"
+                            class="main-content-screenshot"
+                        />
+                        <div>
+                            <h3 class="title">Lorem Ipsum</h3>
+                            <p class="text">
+                                Lorem ipsum dolor sit amet consectetur, adipisicing elit. Amet quae obcaecati vero maiores enim eaque quidem beatae sunt cum nam nesciunt suscipit, tempore officia dolores soluta, ab odit consectetur dignissimos.
+                            </p>
+                            <a
+                                href="https://example.com"
+                                class="btn btn--line"
+                                target="_blank"
+                                >Visit</a
+                            >
+                        </div>
+                    </div>
+                    <div class="main-content">
+                        <img
+                            src="src/assets/car2.jpeg"
+                            width="300"
+                            height="300"
+                            alt="Car 2"
+                            class="main-content-screenshot"
+                        />
+                        <div>
+                        <h3 class="title">Lorem Ipsum</h3>
+                            <p class="text">
+                                Lorem ipsum dolor sit amet consectetur, adipisicing elit. Amet quae obcaecati vero maiores enim eaque quidem beatae sunt cum nam nesciunt suscipit, tempore officia dolores soluta, ab odit consectetur dignissimos.
+                            </p>
+                            <a
+                                href="https://example.com"
+                                class="btn btn--line"
+                                target="_blank"
+                                >Visit</a
+                            >
+                        </div>
+                    </div>
+                    <div class="main-content">
+                        <img
+                            src="src/assets/car3.jpeg"
+                            width="300"
+                            height="300"
+                            alt="Car 3"
+                            class="main-content-screenshot"
+                        />
+                        <div>
+                        <h3 class="title">Lorem Ipsum</h3>
+                            <p class="text">
+                                Lorem ipsum dolor sit amet consectetur, adipisicing elit. Amet quae obcaecati vero maiores enim eaque quidem beatae sunt cum nam nesciunt suscipit, tempore officia dolores soluta, ab odit consectetur dignissimos.
+                            </p>
+                            <a
+                                href="https://example.com"
+                                class="btn btn--line"
+                                target="_blank"
+                                >Visit</a
+                            >
+                        </div>
+                    </div>
+                    <div class="main-content">
+                        <img
+                            src="src/assets/car4.jpeg"
+                            width="300"
+                            height="300"
+                            alt="Car 4"
+                            class="main-content-screenshot"
+                        />
+                        <div>
+                        <h3 class="title">Lorem Ipsum</h3>
+                            <p class="text">
+                                Lorem ipsum dolor sit amet consectetur, adipisicing elit. Amet quae obcaecati vero maiores enim eaque quidem beatae sunt cum nam nesciunt suscipit, tempore officia dolores soluta, ab odit consectetur dignissimos.
+                            </p>
+                            <a
+                                href="https://example.com"
+                                class="btn btn--line"
+                                target="_blank"
+                                >Visit</a
+                            >
+                        </div>
+                    </div>
+                </div>
+            </section>
+    )
+}
