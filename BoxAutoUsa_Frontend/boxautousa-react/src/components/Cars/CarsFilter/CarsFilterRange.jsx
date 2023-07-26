064829e6bb51c4c7d4142f108880c745b18983cc
Vladyslav <5vladik068@gmail.com>
2023-07-26 17:04:18 +0300

    Starting with price range slider

--- BoxAutoUsa_Frontend/boxautousa-react/src/components/Cars/CarsFilter/CarsFilterRange.jsx
+++ BoxAutoUsa_Frontend/boxautousa-react/src/components/Cars/CarsFilter/CarsFilterRange.jsx
@@ -1,13 +1,45 @@
+import { useState } from "react"
+// import Slider from "react-slider"
+
 export default function CarsFilterRange(){
+    const [minPrice, setMinPrice] = useState(0);
+    const [maxPrice, setMaxPrice] = useState(2_000_000)
+
+    const handleSliderChange = (values) => {
+        setMinPrice(values[[0]])
+        setMaxPrice(values[1])
+    }
+
+    const handleMinInputChange = (event) => {
+        setMinPrice(parseInt(event.target.value))
+    }
+
+    const handleMaxInputChange = (event) => {
+        setMaxPrice(parseInt(event.target.value))
+    }
+
     return(
         <div className="filter">
-        <h2 className="title--sm">Car Model</h2>
-    <ul className="filter-category">
-        <li><a href="#" className="filter-link">Aston Martin</a></li>
-        <li><a href="#" className="filter-link">Apple</a></li>
-        <li><a href="#" className="filter-link">BMW</a></li>
-        <li><a href="#" className="filter-link">Some wierd stuff</a></li>
-    </ul>
-    </div>
+            <h2 className="title--sm">Price Range</h2>
+            <p>Min Price: ${minPrice ?? 0}</p>
+            <p>Max Price: ${maxPrice}</p>
+            {/* <Slider
+                min={0}
+                max={100000}
+                step={1000}
+                value={[minPrice, maxPrice]}
+                onChange={handleSliderChange}
+            /> */}
+            <div>
+                <label>
+                Min Price:
+                <input type="number" value={minPrice} onChange={handleMinInputChange} />
+                </label>
+                <label>
+                Max Price:
+                <input type="number" value={maxPrice} onChange={handleMaxInputChange} />
+                </label>
+            </div>
+        </div>
     )
 }
